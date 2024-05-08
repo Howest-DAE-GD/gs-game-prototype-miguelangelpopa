f20934a1811e309efc831d7c029ad37ee9e39586
Miguel Angel Popa <popamiguelangel.04@gmail.com>
2024-05-08 20:29:24 +0200

    Update
    Ik heb een probleem, waardoor op deze engine kan ik mijn game niet runnen. Ik heb de vorige versie gebruikt van de engine en daar werkt het wel.

--- GamePrototype/Game.cpp
+++ GamePrototype/Game.cpp
@@ -1,110 +1,186 @@
 #include "pch.h"
 #include "Game.h"
+#include "utils.h"
+#include <iostream>
+#include "Texture.h"
+#include "SoundEffect.h"
 
-Game::Game( const Window& window ) 
-	:BaseGame{ window }
+Game::Game(const Window& window)
+	:BaseGame{window}
+	,m_Window{ window }
+	, m_MousePos{}
+	, m_pPowerUps{}
+	, m_PowerUpsCenters{}
+	, m_PowerUpTypes{}
+	, m_PowerUpsDestroyed{}
+	, m_PowerUpRadius{}
+	, m_ActNrPowerUps{}
+	, m_Destroyer{ 0.0f,0.0f,20.0f,20.0f }
 {
 	Initialize();
 }
 
-Game::~Game( )
+Game::~Game()
 {
-	Cleanup( );
+	Cleanup();
 }
 
-void Game::Initialize( )
+void Game::Initialize()
+{
+	CreatePowerUps();
+	ShowTestMessage();
+}
+
+
+void Game::Cleanup()
+{
+	DeletePowerUps();
+}
+
+void Game::Update(float elapsedSec)
+{
+	UpdatePowerUps(elapsedSec);
+	MoveDestroyer(m_MousePos);
+	VerifyOverlapping();
+}
+
+void Game::Draw() const
+{
+	ClearBackground();
+	DrawPowerUps();
+	DrawDestroyer();
+}
+
+void Game::ProcessKeyDownEvent(const SDL_KeyboardEvent& e)
+{
+}
+
+void Game::ProcessKeyUpEvent(const SDL_KeyboardEvent& e)
+{
+}
+
+void Game::ProcessMouseMotionEvent(const SDL_MouseMotionEvent& e)
+{
+	m_MousePos.x = float(e.x);
+	m_MousePos.y = m_Window.height - e.y;
+}
+
+void Game::ProcessMouseDownEvent(const SDL_MouseButtonEvent& e)
+{
+}
+
+void Game::ProcessMouseUpEvent(const SDL_MouseButtonEvent& e)
+{
+}
+
+void Game::ClearBackground() const
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+void Game::ShowTestMessage() const
+{
+
+	ShowNrPowerUps();
+}
+void Game::ShowNrPowerUps() const
+{
+	std::cout << "Number of Circle objects " << m_ActNrPowerUps << "\n";
+}
+
+void Game::CreatePowerUps()
 {
 	
+	Texture* pTexture{ new Texture{"./Resources/Images/PowerUp.png" } };
+	m_PowerUpRadius = pTexture->GetWidth() / 2;
+	delete pTexture;
+	pTexture = nullptr;
+
+	int min{ int(m_PowerUpRadius) };
+	int maxX{ int(m_Window.width - m_PowerUpRadius) };
+	int maxY{ int(m_Window.height - m_PowerUpRadius) };
+
+	for (int idx{ 0 }; idx < m_NrPowerUps; ++idx)
+	{
+		Point2f center{ float(rand() % (maxX - min + 1) + min), float(rand() % (maxY - min + 1) + min) };
+		PowerUp::Type type = (idx % 2 == 0) ? PowerUp::Type::green : PowerUp::Type::brown;
+		m_PowerUpsCenters[idx] = center;
+		m_PowerUpTypes[idx] = type;
+		m_PowerUpsDestroyed[idx] = false;
+		m_pPowerUps[idx] = new PowerUp(center, type);
+		++m_ActNrPowerUps;
+	}
 }
 
-void Game::Cleanup( )
+void Game::DeletePowerUps()
 {
+	for (int idx{ 0 }; idx < m_NrPowerUps; ++idx)
+	{
+		delete m_pPowerUps[idx];
+		m_pPowerUps[idx] = nullptr;
+		Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048);
+		m_KillMusic = new SoundEffect("Coin.mp3");
+		m_KillMusic->SetVolume(15);
+		m_KillMusic->Play(1);
+	}
+}
+void Game::DrawPowerUps() const
+{
+	for (int idx{ 0 }; idx < m_NrPowerUps; ++idx)
+	{
+		if (m_pPowerUps[idx] != nullptr)
+		{
+			m_pPowerUps[idx]->Draw();
+			if (m_PowerUpTypes[idx] == PowerUp::Type::brown)
+			{
+				utils::SetColor(Color4f{ 1.0f,0.5f,0.2f,0.5f });
+			}
+			else
+			{
+				utils::SetColor(Color4f{ 0.5f,1.0f,0.5f,0.5f });
+			}
+			utils::FillEllipse(m_PowerUpsCenters[idx], m_PowerUpRadius, m_PowerUpRadius);
+		}
+	}
 }
 
-void Game::Update( float elapsedSec )
+void Game::UpdatePowerUps(float elapsedSec)
 {
-	// Check keyboard state
-	//const Uint8 *pStates = SDL_GetKeyboardState( nullptr );
-	//if ( pStates[SDL_SCANCODE_RIGHT] )
-	//{
-	//	std::cout << "Right arrow key is down\n";
-	//}
-	//if ( pStates[SDL_SCANCODE_LEFT] && pStates[SDL_SCANCODE_UP])
-	//{
-	//	std::cout << "Left and up arrow keys are down\n";
-	//}
+	for (int idx{ 0 }; idx < m_NrPowerUps; ++idx)
+	{
+		if (m_pPowerUps[idx] != nullptr)
+		{
+			m_pPowerUps[idx]->Update(elapsedSec);
+		}
+	}
 }
 
-void Game::Draw( ) const
+void Game::MoveDestroyer(const Point2f& newCenter)
 {
-	ClearBackground( );
+	m_Destroyer.left = newCenter.x - m_Destroyer.width / 2;
+	m_Destroyer.bottom = newCenter.y - m_Destroyer.height / 2;
 }
 
-void Game::ProcessKeyDownEvent( const SDL_KeyboardEvent & e )
+void Game::VerifyOverlapping()
 {
-	//std::cout << "KEYDOWN event: " << e.keysym.sym << std::endl;
+	bool powerUpDeleted{ false };
+	for (int idx{ 0 }; idx < m_NrPowerUps; ++idx)
+	{
+		if (m_pPowerUps[idx] != nullptr && m_pPowerUps[idx]->IsOverlapping(m_Destroyer))
+		{
+			delete m_pPowerUps[idx];
+			m_pPowerUps[idx] = nullptr;
+			--m_ActNrPowerUps;
+			powerUpDeleted = true;
+		}
+	}
+	if (powerUpDeleted)
+	{
+		ShowNrPowerUps();
+	}
 }
-
-void Game::ProcessKeyUpEvent( const SDL_KeyboardEvent& e )
+void Game::DrawDestroyer() const
 {
-	//std::cout << "KEYUP event: " << e.keysym.sym << std::endl;
-	//switch ( e.keysym.sym )
-	//{
-	//case SDLK_LEFT:
-	//	//std::cout << "Left arrow key released\n";
-	//	break;
-	//case SDLK_RIGHT:
-	//	//std::cout << "`Right arrow key released\n";
-	//	break;
-	//case SDLK_1:
-	//case SDLK_KP_1:
-	//	//std::cout << "Key 1 released\n";
-	//	break;
-	//}
-}
-
-void Game::ProcessMouseMotionEvent( const SDL_MouseMotionEvent& e )
-{
-	//std::cout << "MOUSEMOTION event: " << e.x << ", " << e.y << std::endl;
-}
-
-void Game::ProcessMouseDownEvent( const SDL_MouseButtonEvent& e )
-{
-	//std::cout << "MOUSEBUTTONDOWN event: ";
-	//switch ( e.button )
-	//{
-	//case SDL_BUTTON_LEFT:
-	//	std::cout << " left button " << std::endl;
-	//	break;
-	//case SDL_BUTTON_RIGHT:
-	//	std::cout << " right button " << std::endl;
-	//	break;
-	//case SDL_BUTTON_MIDDLE:
-	//	std::cout << " middle button " << std::endl;
-	//	break;
-	//}
-	
-}
-
-void Game::ProcessMouseUpEvent( const SDL_MouseButtonEvent& e )
-{
-	//std::cout << "MOUSEBUTTONUP event: ";
-	//switch ( e.button )
-	//{
-	//case SDL_BUTTON_LEFT:
-	//	std::cout << " left button " << std::endl;
-	//	break;
-	//case SDL_BUTTON_RIGHT:
-	//	std::cout << " right button " << std::endl;
-	//	break;
-	//case SDL_BUTTON_MIDDLE:
-	//	std::cout << " middle button " << std::endl;
-	//	break;
-	//}
-}
-
-void Game::ClearBackground( ) const
-{
-	glClearColor( 0.0f, 0.0f, 0.3f, 1.0f );
-	glClear( GL_COLOR_BUFFER_BIT );
+	utils::SetColor(Color4f{ 1.0f,0.0f,0.0f,1.0f });
+	utils::FillRect(m_Destroyer);
 }
--- GamePrototype/Game.h
+++ GamePrototype/Game.h
@@ -1,30 +1,59 @@
 #pragma once
 #include "BaseGame.h"
+#include "SoundEffect.h"
+
+class PowerUp;
 class Game : public BaseGame
 {
 public:
-	explicit Game( const Window& window );
+	explicit Game(const Window& window);
 	Game(const Game& other) = delete;
 	Game& operator=(const Game& other) = delete;
-	Game( Game&& other) = delete;
+	Game(Game&& other) = delete;
 	Game& operator=(Game&& other) = delete;
 	// http://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rh-override
 	~Game();
 
-	void Update( float elapsedSec ) override;
-	void Draw( ) const override;
+	void Update(float elapsedSec) override;
+	void Draw() const override;
 
 	// Event handling
-	void ProcessKeyDownEvent( const SDL_KeyboardEvent& e ) override;
-	void ProcessKeyUpEvent( const SDL_KeyboardEvent& e ) override;
-	void ProcessMouseMotionEvent( const SDL_MouseMotionEvent& e ) override;
-	void ProcessMouseDownEvent( const SDL_MouseButtonEvent& e ) override;
-	void ProcessMouseUpEvent( const SDL_MouseButtonEvent& e ) override;
+	void ProcessKeyDownEvent(const SDL_KeyboardEvent& e) override;
+	void ProcessKeyUpEvent(const SDL_KeyboardEvent& e) override;
+	void ProcessMouseMotionEvent(const SDL_MouseMotionEvent& e) override;
+	void ProcessMouseDownEvent(const SDL_MouseButtonEvent& e) override;
+	void ProcessMouseUpEvent(const SDL_MouseButtonEvent& e) override;
 
 private:
 
+	// DATA MEMBERS
+	const Window m_Window;
+
+	Point2f m_MousePos;
+	static const int m_NrPowerUps{ 10 };
+	PowerUp* m_pPowerUps[m_NrPowerUps];
+	Point2f m_PowerUpsCenters[m_NrPowerUps];
+	PowerUp::Type m_PowerUpTypes[m_NrPowerUps];
+	bool m_PowerUpsDestroyed[m_NrPowerUps];
+	float m_PowerUpRadius;
+	int m_ActNrPowerUps;
+	Rectf m_Destroyer;
+	SoundEffect* m_KillMusic;
+
 	// FUNCTIONS
 	void Initialize();
-	void Cleanup( );
-	void ClearBackground( ) const;
+	void Cleanup();
+	void ClearBackground() const;
+
+	void ShowTestMessage() const;
+	void ShowNrPowerUps() const;
+	void CreatePowerUps();
+	void UpdatePowerUps(float elapsedSec);
+	void DrawPowerUps() const;
+	void DeletePowerUps();
+
+	void MoveDestroyer(const Point2f& newCenter);
+	void DrawDestroyer() const;
+	void VerifyOverlapping();
+
 };
